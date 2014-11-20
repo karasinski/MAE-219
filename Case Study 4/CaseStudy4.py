c826e0db3c2be096c7bd132cc7775f2c7c2f3efc
John Karasinski <karasinski@gmail.com>
2014-11-20 11:35:24 -0800

    reduce wordiness; fix boundary conditions

--- Case Study 4/CaseStudy4.py
+++ Case Study 4/CaseStudy4.py
@@ -26,30 +26,30 @@ class Config(object):
 
 
 def generate_things(C, s):
-    constants = Config(C, s)
+    c = Config(C, s)
 
     # Initial Condition with boundary conditions
-    T_initial = [np.sin(constants.k * x_i) for x_i in constants.x]
+    T_initial = [np.sin(c.k * x_i) for x_i in c.x]
 
     # Analytic Solution
-    T_analytic = Analytic(constants)
+    T_analytic = Analytic(c)
     plt.plot(T_analytic, label='Analytic')
 
     # Explicit Numerical Solution
-    T_explicit = Explicit(T_initial, constants)
+    T_explicit = Explicit(T_initial, c)
     plt.plot(T_explicit, label='Explicit')
     plt.legend()
-    # plt.show()
+    plt.show()
 
     err = T_explicit - T_analytic
     RMS = np.sqrt(np.mean(np.square(err)))
 
-    print('dx: ' + str(constants.dx) + ', dt: ' + str(constants.dt))
-    return [constants.dx, constants.dt, RMS]
+    print('dx: ' + str(c.dx) + ', dt: ' + str(c.dt))
+    return [c.dx, c.dt, RMS]
 
 
-def Analytic(config):
-    k, D, u, tau, x = config.k, config.D, config.u, config.tau, config.x
+def Analytic(c):
+    k, D, u, tau, x = c.k, c.D, c.u, c.tau, c.x
 
     N = len(x)
     t = 1 / tau
@@ -61,22 +61,25 @@ def Analytic(config):
     return np.array(T)
 
 
-def Explicit(T, config):
+def Explicit(T, c):
     """
     FTCS (Explicit) - Forward-Time and central differencing for both the
     convective flux and the diffusive flux.
     """
 
-    D, dt, dx, u, tau, k, L = config.D, config.dt, config.dx, config.u, config.tau, config.k, config.L
+    D, dt, dx, u, tau, k = c.D, c.dt, c.dx, c.u, c.tau, c.k
 
     N = len(T)
     T = np.array(T)
     T_old = np.array(T)
 
+    spatial_stability = dx < (2 * D) / u
+    temporal_stability = dt < dx ** 2 / (2 * D)
+    print('Stability: spatial ' + str(spatial_stability) +
+                  ', temporal ' + str(temporal_stability))
+
     t = 0
     while t < 1 / tau:
-        T[0] = np.sin(k * 0)
-        T[-1] = np.sin(k * L)
         for i in range(1, N - 1):
             T[i] = ((1 - 2 * D * dt / dx ** 2) * T_old[i] +
                     (D * dt / dx ** 2 + u * dt / (2 * dx)) * T_old[i - 1] +
@@ -85,7 +88,11 @@ def Explicit(T, config):
         T_old = np.array(T)
         t += dt
 
-    return np.array(T)
+        # Enforce our periodic boundary condition
+        T_old[0] = np.exp(-k ** 2 * D * t) * np.sin(-k * u * t)
+        T_old[-1] = T_old[0]
+
+    return np.array(T_old)
 
 
 def main():
@@ -98,9 +105,9 @@ def main():
         # print('C: ' + str(C_i) + ', s: ' + str(s_i))
         results.append([dx, dt, RMS])
 
-    print(results)
+    # print(results)
     results.sort(key=lambda x: x[0])
-    print(results)
+    # print(results)
     results = np.array(results)
 
     plt.subplot(2, 1, 1)
@@ -108,20 +115,20 @@ def main():
     plt.title('dx vs RMS')
     plt.yscale('log')
 
-    logx = scipy.log10(results[:, 0])
-    logy = scipy.log10(results[:, 2])
+    # logx = scipy.log10(results[:, 0])
+    # logy = scipy.log10(results[:, 2])
 
-    # define our (line) fitting function
-    def linear_fit(x, a, b):
-        return a + b * x
+    # # define our (line) fitting function
+    # def linear_fit(x, a, b):
+    #     return a + b * x
 
-    out = curve_fit(linear_fit, logx, logy)
-    fit = linear_fit(results[:, 0], out[0][0], out[0][1])
-    # pfinal = out[0]
-    # covar = out[1]
-    # print(pfinal, covar)
-    plt.plot(results[:, 0], fit)
-    print(out)
+    # out = curve_fit(linear_fit, logx, logy)
+    # fit = linear_fit(results[:, 0], out[0][0], out[0][1])
+    # # pfinal = out[0]
+    # # covar = out[1]
+    # # print(pfinal, covar)
+    # plt.plot(results[:, 0], fit)
+    # print(out)
 
     plt.subplot(2, 1, 2)
     plt.plot(results[:, 1], results[:, 2], '.')
