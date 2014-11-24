7470f55ef90c9deb525afd2f8851c24f82f1e4cc
John Karasinski <karasinski@gmail.com>
2014-11-23 17:55:43 -0800

    formatting

--- Case Study 4/CaseStudy4.py
+++ Case Study 4/CaseStudy4.py
@@ -93,7 +93,7 @@ def Explicit(T, c):
     # spatial_stability = dx < (2 * D) / u
     # temporal_stability = dt < dx ** 2 / (2 * D)
     # print('Stability: spatial ' + str(spatial_stability) +
-                  # ', temporal ' + str(temporal_stability))
+    # ', temporal ' + str(temporal_stability))
 
     t = 0
     while t < tau:
@@ -104,8 +104,8 @@ def Explicit(T, c):
 
         # Enforce our periodic boundary condition
         T[-1] = ((1 - 2 * D * dt / dx ** 2) * T_old[-1] +
-                (D * dt / dx ** 2 + u * dt / (2 * dx)) * T_old[-2] +
-                (D * dt / dx ** 2 - u * dt / (2 * dx)) * T_old[1])
+                 (D * dt / dx ** 2 + u * dt / (2 * dx)) * T_old[-2] +
+                 (D * dt / dx ** 2 - u * dt / (2 * dx)) * T_old[1])
         T[0] = T[-1]
 
         T_old = np.array(T)
@@ -152,25 +152,6 @@ def Upwind(T, c):
 
     return np.array(T_old)
 
-    # while t <= tau:
-    #     # first two values
-    #     T[0] = dt*(D*(T_old[1]-2*T_old[0]+T_old[N-1])/(dx**2) - u*(3*T_old[0]-4*T_old[N-1]+T_old[N-2])/(2*dx)) + T_old[0]
-    #     T[1] = dt*(D*(T_old[2]-2*T_old[1]+T_old[0])/(dx**2) - u*(3*T_old[1]-4*T_old[0]+T_old[N-1])/(2*dx)) + T_old[1]
-
-    #     # Center values
-    #     for i in range(2,N-1):
-    #         T[i] = dt*(D*(T_old[i+1]-2*T_old[i]+T_old[i-1])/(dx**2) - u*(3*T_old[i]-4*T_old[i-1]+T_old[i-2])/(2*dx)) + T_old[i]
-
-    #     # Last value
-    #     T[N-1] = dt*(D*(T_old[0]-2*T_old[N-1]+T_old[N-2])/(dx**2) - u*(3*T_old[N-1]-4*T_old[N-2]+T_old[N-3])/(2*dx)) + T_old[N-1]
-
-    #     # Increment
-    #     T_old = np.array(T)
-    #     t += dt
-
-    # return np.array(T_old)
-
-
 
 def Trapezoidal(T, c):
     D, dt, dx, u, tau = c.D, c.dt, c.dx, c.u, c.tau
@@ -179,17 +160,17 @@ def Trapezoidal(T, c):
     T = np.array(T)
 
     # Create Coefficient Matrix
-    main = [1+(dt*D/(dx**2)) for i in range(0,N)]
-    upper = [-(dt*D)/(2*dx**2) + dt*u/(4*dx) for i in range(0,N)]
-    lower = [-(dt*D)/(2*dx**2) - dt*u/(4*dx) for i in range(0,N)]
+    upper = [-(dt * D) / (2 * dx ** 2) + dt * u / (4 * dx) for i in range(0, N)]
+    main = [1 + (dt * D / (dx ** 2)) for i in range(0, N)]
+    lower = [-(dt * D) / (2 * dx ** 2) - dt * u / (4 * dx) for i in range(0, N)]
 
-    data = lower,main,upper
-    diags = np.array([-1,0,1])
-    matrix = sparse.spdiags(data,diags,N,N).todense()
+    data = lower, main, upper
+    diags = np.array([-1, 0, 1])
+    matrix = sparse.spdiags(data, diags, N, N).todense()
 
     # Set values for cyclic boundary conditions
-    matrix[0,N-1] = -(dt*D)/(2*dx**2) - dt*u/(4*dx)
-    matrix[N-1,0] = -(dt*D)/(2*dx**2) + dt*u/(4*dx)
+    matrix[0, N - 1] = -(dt * D) / (2 * dx ** 2) - dt * u / (4 * dx)
+    matrix[N - 1, 0] = -(dt * D) / (2 * dx ** 2) + dt * u / (4 * dx)
 
     T_old = np.array(T)
 
@@ -200,16 +181,19 @@ def Trapezoidal(T, c):
     while t <= tau:
         # create b array
         # First value
-        b[0] = (dt*D/(2*dx**2))*(T_old[1]-2*T_old[0]+T_old[N-1]) - (u*dt/(4*dx))*(T_old[1]-T_old[N-1]) + T_old[0]
+        b[0] = ((dt * D / (2 * dx ** 2)) * (T_old[1] - 2 * T_old[0] + T_old[N - 1]) -
+                (u * dt / (4 * dx)) * (T_old[1] - T_old[N - 1]) + T_old[0])
 
-        for i in range(1,N-1):
-            b[i] = (dt*D/(2*dx**2))*(T_old[i+1]-2*T_old[i]+T_old[i-1]) - (u*dt/(4*dx))*(T_old[i+1]-T_old[i-1]) + T_old[i]
+        for i in range(1, N - 1):
+            b[i] = ((dt * D / (2 * dx ** 2)) * (T_old[i + 1] - 2 * T_old[i] + T_old[i - 1]) -
+                    (u * dt / (4 * dx)) * (T_old[i + 1] - T_old[i - 1]) + T_old[i])
 
-        #last value
-        b[N-1] = (dt*D/(2*dx**2))*(T_old[0]-2*T_old[N-1]+T_old[N-2]) - (u*dt/(4*dx))*(T_old[0]-T_old[N-2]) + T_old[N-1]
+        # last value
+        b[N - 1] = ((dt * D / (2 * dx ** 2)) * (T_old[0] - 2 * T_old[N - 1] + T_old[N - 2]) -
+                    (u * dt / (4 * dx)) * (T_old[0] - T_old[N - 2]) + T_old[N - 1])
 
         # Solve matrix
-        T = np.linalg.solve(matrix,b)
+        T = np.linalg.solve(matrix, b)
 
         T_old = np.array(T)
         t += dt
@@ -227,14 +211,18 @@ def QUICK(T, c):
     t = 0
     while t <= tau:
         # First two points
-        T[0] = dt*(D*(T_old[1]-2*T_old[0]+T_old[N-1])/(dx**2) - u*(3*T_old[1]-T_old[N-2]+6*T_old[N-1]-8*T_old[0])/(8*dx)) + T_old[0]
-        T[1] = dt*(D*(T_old[2]-2*T_old[1]+T_old[0])/(dx**2) - u*(3*T_old[2]-T_old[N-1]+6*T_old[0]-8*T_old[1])/(8*dx)) + T_old[1]
+        T[0] = (dt * (D * (T_old[1] - 2 * T_old[0] + T_old[N - 1]) / (dx ** 2) - u *
+                      (3 * T_old[1] - T_old[N - 2] + 6 * T_old[N - 1] - 8 * T_old[0]) / (8 * dx)) + T_old[0])
+        T[1] = (dt * (D * (T_old[2] - 2 * T_old[1] + T_old[0]) / (dx ** 2) - u *
+                      (3 * T_old[2] - T_old[N - 1] + 6 * T_old[0] - 8 * T_old[1]) / (8 * dx)) + T_old[1])
 
-        for i in range(2,N-1):
-            T[i] = dt*(D*(T_old[i+1]-2*T_old[i]+T_old[i-1])/(dx**2) - u*(3*T_old[i+1]-T_old[i-2]+6*T_old[i-1]-8*T_old[i])/(8*dx)) + T_old[i]
+        for i in range(2, N - 1):
+            T[i] = (dt * (D * (T_old[i + 1] - 2 * T_old[i] + T_old[i - 1]) / (dx ** 2) - u *
+                          (3 * T_old[i + 1] - T_old[i - 2] + 6 * T_old[i - 1] - 8 * T_old[i]) / (8 * dx)) + T_old[i])
 
         # Last point
-        T[N-1] = dt*(D*(T_old[0]-2*T_old[N-1]+T_old[N-2])/(dx**2) - u*(3*T_old[0]-T_old[N-3]+6*T_old[N-2]-8*T_old[N-1])/(8*dx)) + T_old[N-1]
+        T[N - 1] = (dt * (D * (T_old[0] - 2 * T_old[N - 1] + T_old[N - 2]) / (dx ** 2) - u *
+                          (3 * T_old[0] - T_old[N - 3] + 6 * T_old[N - 2] - 8 * T_old[N - 1]) / (8 * dx)) + T_old[N - 1])
 
         # Increment
         T_old = np.array(T)
@@ -268,36 +256,36 @@ def main():
         # print('C: ' + str(C_i) + ', s: ' + str(s_i))
         results.append([dx, dt, RMS])
 
-    # # Sort and convert
-    # results.sort(key=lambda x: x[0])
-    # results = np.array(results)
+    # Sort and convert
+    results.sort(key=lambda x: x[0])
+    results = np.array(results)
 
-    # # Pull out data
-    # x = results[:, 0]
-    # t = results[:, 1]
-    # RMS = results[:, 2]
+    # Pull out data
+    x = results[:, 0]
+    t = results[:, 1]
+    RMS = results[:, 2]
 
-    # # Find effective order of accuracy
-    # order_accuracy_x = effective_order(x, RMS)
-    # order_accuracy_t = effective_order(t, RMS)
-    # # print('x order: ', order_accuracy_x, 't order: ', order_accuracy_t)
+    # Find effective order of accuracy
+    order_accuracy_x = effective_order(x, RMS)
+    order_accuracy_t = effective_order(t, RMS)
+    print('x order: ', order_accuracy_x, 't order: ', order_accuracy_t)
 
-    # # Show effect of dx on RMS
-    # plt.subplot(2, 1, 1)
-    # plt.plot(x, RMS, '.')
-    # plt.title('dx vs RMS, effective order {0:1.2f}'.format(order_accuracy_x))
-    # plt.xscale('log')
-    # plt.yscale('log')
+    # Show effect of dx on RMS
+    plt.subplot(2, 1, 1)
+    plt.plot(x, RMS, '.')
+    plt.title('dx vs RMS, effective order {0:1.2f}'.format(order_accuracy_x))
+    plt.xscale('log')
+    plt.yscale('log')
 
-    # # Show effect of dt on RMS
-    # plt.subplot(2, 1, 2)
-    # plt.plot(t, RMS, '.')
-    # plt.title('dt vs RMS, effective order {0:1.2f}'.format(order_accuracy_t))
-    # plt.xscale('log')
-    # plt.yscale('log')
+    # Show effect of dt on RMS
+    plt.subplot(2, 1, 2)
+    plt.plot(t, RMS, '.')
+    plt.title('dt vs RMS, effective order {0:1.2f}'.format(order_accuracy_t))
+    plt.xscale('log')
+    plt.yscale('log')
 
-    # # Finally show it off
-    # plt.show()
+    # Finally show it off
+    plt.show()
 
 
 if __name__ == "__main__":
