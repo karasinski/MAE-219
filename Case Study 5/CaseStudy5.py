e4d9b2fb6d87dcd79bd2e06dc42d52d98a62b07c
John Karasinski <karasinski@gmail.com>
2014-12-06 22:28:07 -0800

    basic solution complete

--- Case Study 5/CaseStudy5.py
+++ Case Study 5/CaseStudy5.py
@@ -1,7 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import ode
 from time import clock
+from scipy.sparse import spdiags
+import json
+from PrettyPlots import *
 
 
 def K(z):
@@ -14,17 +16,8 @@ def gamma(z):
 
 def R(y_1, y_2, t):
     '''
-    Find the reaction rate of the system at state c and time t.
+    Find the reaction rates, R_1 and R_2, of the system at state c and time t.
     '''
-
-    y_3 = 3.7e16          # Concentration of O_2 (constant)
-
-    k_1 = 1.63E-16       # Reaction rate [O + O_2 ->     O_3]
-    k_2 = 4.66E-16       # Reaction rate [O + O_3 -> 2 * O_2]
-
-    a_3 = 22.62          # Constant used in calculation of k_3
-    a_4 = 7.601          # Constant used in calculation of k_4
-    w = np.pi / 43200    # Cycle (half a day) [1/sec]
 
     if np.sin(w * t) > 0:
         k_3 = np.exp(-a_3 / np.sin(w * t))
@@ -42,42 +35,38 @@ def system(t, y):
     f = np.zeros(len(y))
 
     R1, R2 = R(y[0], y[1], t)
-    f[0] = (dz ** -2 * (K(3. / 2.) * y[2] - (K(3. / 2.) + K(1. / 2.)) * y[0] + K(1. / 2.) * y[2]) + R1)
-    f[1] = (dz ** -2 * (K(3. / 2.) * y[3] - (K(3. / 2.) + K(1. / 2.)) * y[1] + K(1. / 2.) * y[3]) + R2)
+    l_p, l_m = 3. / 2., 1. / 2.
+
+    f[0] = (dz ** -2 * (K(l_p) * y[2] - (K(l_p) + K(l_m)) * y[0] + K(l_m) * y[2]) + R1)
+    f[1] = (dz ** -2 * (K(l_p) * y[3] - (K(l_p) + K(l_m)) * y[1] + K(l_m) * y[3]) + R2)
 
     for i in range(1, M):
         R1, R2 = R(y[2 * i], y[2 * i + 1], t)
-        f[2 * i] =     (dz ** -2 * (K(i + 1. + 1. / 2.) * y[2 * i + 2] - (K(i + 1. + 1. / 2.) + K(i + 1 - 1. / 2.)) * y[2 * i]     + K(i + 1 - 1. / 2.) * y[2 * i - 2]) + R1)
-        f[2 * i + 1] = (dz ** -2 * (K(i + 1. + 1. / 2.) * y[2 * i + 3] - (K(i + 1. + 1. / 2.) + K(i + 1 - 1. / 2.)) * y[2 * i + 1] + K(i + 1 - 1. / 2.) * y[2 * i - 1]) + R2)
+        l_p, l_m = i + 3. / 2., i + 1. / 2.
+
+        f[2 * i] =     (dz ** -2 * (K(l_p) * y[2 * i + 2] - (K(l_p) + K(l_m)) * y[2 * i]     + K(l_m) * y[2 * i - 2]) + R1)
+        f[2 * i + 1] = (dz ** -2 * (K(l_p) * y[2 * i + 3] - (K(l_p) + K(l_m)) * y[2 * i + 1] + K(l_m) * y[2 * i - 1]) + R2)
 
     R1, R2 = R(y[2 * M - 2], y[2 * M - 1], t)
-    f[-2] = (dz ** -2 * (K(M - 1. / 2.) * y[2 * M - 4] - (K(M - 1. / 2.) + K(M - 3. / 2.)) * y[2 * M - 2] + K(M - 3. / 2.) * y[2 * M - 4]) + R1)
-    f[-1] = (dz ** -2 * (K(M - 1. / 2.) * y[2 * M - 3] - (K(M - 1. / 2.) + K(M - 3. / 2.)) * y[2 * M - 1] + K(M - 3. / 2.) * y[2 * M - 3]) + R2)
+    l_p, l_m = M + 1. / 2., M - 1. / 2.
+
+    f[-2] = (dz ** -2 * (K(l_p) * y[2 * M - 4] - (K(l_p) + K(l_m)) * y[2 * M - 2] + K(l_m) * y[2 * M - 4]) + R1)
+    f[-1] = (dz ** -2 * (K(l_p) * y[2 * M - 3] - (K(l_p) + K(l_m)) * y[2 * M - 1] + K(l_m) * y[2 * M - 3]) + R2)
 
     return f
 
 
 def jacobian(t, y):
-    main  = np.zeros(len(y))
-    sub_1 = np.zeros(len(y))
-    sub_2 = np.zeros(len(y))
-    sup_1 = np.zeros(len(y))
-    sup_2 = np.zeros(len(y))
-
-    y_3 = 3.7e16          # Concentration of O_2 (constant)
-
-    k_1 = 1.63E-16       # Reaction rate [O + O_2 ->     O_3]
-    k_2 = 4.66E-16       # Reaction rate [O + O_3 -> 2 * O_2]
-
-    a_4 = 7.601          # Constant used in calculation of k_4
-    w = np.pi / 43200    # Cycle (half a day) [1/sec]
+    main = np.zeros(len(y))
+    sub_1, sub_2 = np.zeros(len(y)), np.zeros(len(y))
+    sup_1, sup_2 = np.zeros(len(y)), np.zeros(len(y))
 
     if np.sin(w * t) > 0:
         k_4 = np.exp(-a_4 / np.sin(w * t))
     else:
         k_4 = 0
 
-    for i in range(0, M + 1, 2):
+    for i in range(0, M + 1):
         main[2 * i] =     -dz ** -2 * (K(i + 1. + 1./2.) + K(i + 1 - 1./2.)) - k_1 * y_3 - k_2 * y[2 * i + 1]
         main[2 * i + 1] =                                                                - k_2 * y[2 * i]     - k_4
 
@@ -90,83 +79,107 @@ def jacobian(t, y):
         sub_1[2 * i + 1] = 0
 
     sup_2[-2] = dz ** -2 * (K(i + 1. + 1./2.) + K(i + 1 - 1./2.))
-    sup_2[-1] = dz ** -2 * (K(i + 1. + 1./2.) + K(i + 1 - 1./2.))
+    sup_2[-1] = sup_2[-2]
     for i in range(2, 2 * M):
-        sup_2[i] = dz ** -2 *  K(i + 1. + 1./2.)
+        sup_2[i] = dz ** -2 * K(i + 1. + 1./2.)
 
-    sub_2[0:2] = dz ** -2 *  K(i + 1. - 1./2.)
+    sub_2[0:2] = dz ** -2 * K(i + 1. - 1./2.)
     for i in range(2, 2 * M):
         sub_2[i] = dz ** -2 * (K(i + 1. + 1./2.) + K(i + 1 - 1./2.))
 
-    jac = np.vstack((sup_2, sup_1, main, sub_1, sub_2))
-    print(jac)
+    diag_rows = np.array([sup_2, sup_1, main, sub_1, sub_2])
+    positions = [2, 1, 0, -1, -2]
+    jac = spdiags(diag_rows, positions, len(y), len(y)).todense()
+
+    return jac
 
 
-# Basic parameters
+def solve(solver, c, integrator):
+    # Create result arrays
+    c1, c2 = [], []
+
+    start_time = clock()
+    for i in range(0, len(times) - 1):
+        # Initial and final time
+        t_0 = times[i]
+        t_f = times[i + 1]
+
+        # Solver setup
+        t = []
+        sol = []
+        solver.set_initial_value(c, t_0)
+        while solver.successful() and solver.t < t_f:
+            solver.integrate(solver.t + dt)
+            t.append(solver.t)
+            sol.append(solver.y)
+
+        t = np.array(t)
+        sol = np.array(sol)
+
+        # Save c1, c2 solutions
+        y_sol = sol[-1]
+        c1.append(y_sol[0::2])
+        c2.append(y_sol[1::2])
+
+        #Update initial conditions for next iteration
+        c = y_sol
+
+    elapsed_time = clock() - start_time
+    print(elapsed_time, "seconds process time")
+
+    output = [c1, c2]
+    return output
+
+
+def run_trials():
+    number_of_solvers = 4
+    for trial in range(number_of_solvers):
+        # Set up ODE solver
+        if trial == 0:
+            solver = ode(system)
+            integrator = 'dop853'
+            solver.set_integrator(integrator, atol=1E-1, rtol=1E-3)
+        elif trial == 1:
+            solver = ode(system)
+            integrator = 'dopri5'
+            solver.set_integrator(integrator, atol=1E-1, rtol=1E-3)
+        elif trial == 2:
+            solver = ode(system)
+            integrator = 'bdf'
+            solver.set_integrator('vode', method=integrator, atol=1E-1, rtol=1E-3)
+        elif trial == 4:
+            solver = ode(system, jacobian)
+            integrator = 'bdf Jacobian'
+            solver.set_integrator('vode', method=integrator.split(' ')[0], atol=1E-1, rtol=1E-3, with_jacobian=True)
+
+        print("Starting solver: ", integrator)
+        c1, c2 = solve(solver, c, integrator)
+
+        plot_c1(z, c, c1, labels, integrator)
+        plot_c2(z, c, c2, labels, integrator)
+
+# Basic problem parameters
 M = 50
 dz = 20. / M
 z = [30. + j * dz for j in range(M + 1)]
 
+y_3 = 3.7E16         # Concentration of O_2 (constant)
+k_1 = 1.63E-16       # Reaction rate [O + O_2 ->     O_3]
+k_2 = 4.66E-16       # Reaction rate [O + O_3 -> 2 * O_2]
+a_3 = 22.62          # Constant used in calculation of k_3
+a_4 = 7.601          # Constant used in calculation of k_4
+w = np.pi / 43200.   # Cycle (half a day) [1/sec]
+
 # This generates the initial conditions
-c = np.zeros(len(2*z))
+c = np.zeros(len(2 * z))
 for i, _ in enumerate(z):
-    c[2 * i] = 1E6 * gamma(z[i])
+    c[2 * i]     = 1E6  * gamma(z[i])
     c[2 * i + 1] = 1E12 * gamma(z[i])
 
 # Time array
 times = 3600. * np.array([0., 2., 4., 6., 7., 9., 12., 18., 24.])
-dt = 120.
+dt = 60.
 
-# Create result arrays
-c1, c2 = [], []
+labels = [str(int(x / 3600.)) + " hours" for x in times[1:]]
 
-# Set up ODE solver
-solver = ode(system)
-
-jacobian(0, c)
-
-
-for i in range(0, len(times) - 1):
-    # Initial and final time
-    t_0 = times[i]
-    t_f = times[i + 1]
-
-    # Solver setup
-    t = []
-    sol = []
-    solver.set_initial_value(c, t_0)
-    # solver.set_integrator('dop853')
-    # solver.set_integrator('dopri5')
-    solver.set_integrator('vode', method='bdf')
-    # with_jacobian=True
-    start_time = clock()
-    while solver.successful() and solver.t < t_f:
-        solver.integrate(solver.t + dt)
-        t.append(solver.t)
-        sol.append(solver.y)
-    print clock() - start_time, "seconds process time"
-
-    t = np.array(t)
-    sol = np.array(sol)
-
-    # Set c1, c2 values
-    y_sol = sol[-1]
-    c1.append(y_sol[0::2])
-    c2.append(y_sol[1::2])
-
-    #Update IC for next iteration
-    c = y_sol
-
-fig = plt.figure()
-ax1 = fig.add_subplot(2, 1, 1)
-for solution in c1:
-    plt.plot(z, solution)
-plt.ylabel('$c_1$')
-ax1.set_xticklabels([])
-
-ax2 = fig.add_subplot(2, 1, 2)
-for solution in c2:
-    plt.plot(z, solution)
-plt.ylabel('$c_2$')
-plt.xlabel('z (km)')
-plt.show()
+run_trials()
---
+++ Case Study 5/PrettyPlots.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import os
+
+# Configure figures for production
+WIDTH = 495.0  # the number latex spits out
+FACTOR = 1.0   # the fraction of the width the figure should occupy
+fig_width_pt = WIDTH * FACTOR
+
+inches_per_pt = 1.0 / 72.27
+golden_ratio = (np.sqrt(5) - 1.0) / 2.0      # because it looks good
+fig_width_in = fig_width_pt * inches_per_pt  # figure width in inches
+fig_height_in = fig_width_in * golden_ratio   # figure height in inches
+fig_dims = [fig_width_in, fig_height_in]  # fig dims as a list
+
+
+def save_plot(save_name):
+    # Save plots
+    try:
+        os.mkdir('figures')
+    except Exception:
+        pass
+
+    plt.savefig('figures/' + save_name, bbox_inches='tight')
+    plt.close()
+
+
+def plot_c1(z, initial, c1, labels, integrator):
+    plt.figure(figsize=fig_dims)
+    plt.plot(z, initial[0::2], label='0 hours')
+    for solution, label in zip(c1, labels):
+        if "12" in label:
+            break
+        plt.plot(z, solution, label=label)
+    plt.ylabel('$c_1$')
+    plt.xlabel('z (km)')
+    plt.yscale('log')
+    plt.legend()
+    save_name = integrator + ' c1.pdf'
+    save_plot(save_name)
+
+
+def plot_c2(z, initial, c2, labels, integrator):
+    plt.figure(figsize=fig_dims)
+    plt.plot(z, initial[1::2], label='0 hours')
+    for solution, label in zip(c2, labels):
+        plt.plot(z, solution, label=label)
+    plt.ylabel('$c_2$')
+    plt.xlabel('z (km)')
+    # plt.yscale('log')
+    plt.legend()
+    save_name = integrator + ' c2.pdf'
+    save_plot(save_name)
