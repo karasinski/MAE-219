87e60683c6a0af246023494ea8eaa7205d0902d5
John Karasinski <karasinski@gmail.com>
2014-12-19 20:10:13 -0800

    add conclusions

--- Final/code/Plots.py
+++ Final/code/Plots.py
@@ -11,59 +11,57 @@ fig_width_pt = WIDTH * FACTOR
 inches_per_pt = 1.0 / 72.27
 golden_ratio = (np.sqrt(5) - 1.0) / 2.0      # because it looks good
 fig_width_in = fig_width_pt * inches_per_pt  # figure width in inches
-fig_height_in = fig_width_in * golden_ratio   # figure height in inches
-fig_dims = [fig_width_in, fig_height_in]  # fig dims as a list
+fig_height_in = fig_width_in * golden_ratio  # figure height in inches
+fig_dims = [fig_width_in, fig_height_in]     # fig dims as a list
 
-arr_befor = []
-arr_after = []
-for i in range(201):
-    j = str(i)
-
-    # Load data
-    path = 'M=100/Run0.5-100.0/postProcessing/sets/' + j + '/'
-    before = np.loadtxt(path + 'before_Ux_Uy.xy', delimiter=' ', unpack=True)
-    after  = np.loadtxt(path + 'after_Ux_Uy.xy',  delimiter=' ', unpack=True)
-
-    Y, UX, UY = 0, 1, 2
-    before_Y,  after_Y  = before[Y],  after[Y]
-    before_UX, after_UX = before[UX], after[UX]
-    # before_UY, after_UY = before[UY], after[UY]
-
-    # Final flow should be linear
-    linear = np.linspace(0, 1, len(before_UX))
-
-    # Calculate RMS
-    err_befor, err_after = before_UX - linear, after_UX - linear
-    arr_befor.append(np.sqrt(np.mean(np.square(err_befor))))
-    arr_after.append(np.sqrt(np.mean(np.square(err_after))))
-
-
-# Plot
+ARs = ["0.5", "2.0", "5.0"]
+colors = ['r', 'g', 'b']
 plt.figure(figsize=fig_dims)
-plt.plot(arr_befor, 'b+', label='before')
-plt.plot(arr_after, 'rx', label='after')
-plt.yscale('log')
-plt.legend()
-plt.xlabel('Time (s)')
-plt.ylabel('NRMS')
+for AR, color in zip(ARs, colors):
+    arr_befor = []
+    arr_after = []
+    for i in range(150):
+        # Load data
+        path = 'M=100/Run' + AR + '-100.0/postProcessing/sets/' + str(i) + '/'
+        before = np.loadtxt(path + 'before_Ux_Uy.xy', delimiter=' ', unpack=True)
+        after  = np.loadtxt(path + 'after_Ux_Uy.xy',  delimiter=' ', unpack=True)
 
-# plt.figure(figsize=fig_dims)
-# plt.subplot(2, 1, 1)
+        Y, UX = 0, 1
+        before_Y,  after_Y  = before[Y],  after[Y]
+        before_UX, after_UX = before[UX], after[UX]
 
-# plt.plot(befor_error, before_Y, 'b+', label='before')
-# plt.plot(after_error,  after_Y, 'rx', label='after')
-# # plt.plot(middle_Y, middle_UX,  'g:', label='middle')
-# plt.ylabel('y (m)')
-# plt.xlabel('Error in Ux (m/s)')
-# # plt.xlim([0, 1])
-# plt.legend(loc='upper left')
+        # Final flow should be linear
+        linear = np.linspace(0, 1, len(before_UX))
 
-# plt.subplot(2, 1, 2)
-# plt.plot(before_Y, before_UY, 'b+',  label='before')
-# plt.plot(after_Y,  after_UY,  'rx', label='after')
-# # plt.plot(middle_Y, middle_UY,  'g:', label='middle')
-# plt.xlabel('Y')
-# plt.ylabel('Uy')
-# plt.xlim([0, 1])
-# plt.legend(loc='upper left')
+        # Calculate RMS
+        err_befor, err_after = before_UX - linear, after_UX - linear
+        arr_befor.append(np.sqrt(np.mean(np.square(err_befor))))
+        arr_after.append(np.sqrt(np.mean(np.square(err_after))))
+
+    # Plot
+    plt.plot(arr_befor, color + '+', label='AR=' + AR + ', before')
+    plt.plot(arr_after, color + 'x', label='AR=' + AR + ', after')
+    plt.yscale('log')
+    plt.legend()
+    plt.xlim([0, 150])
+    plt.xlabel('Time (s)')
+    plt.ylabel('NRMS')
+
+plt.show()
+plt.close()
+
+path = 'M=100/Run0.5-100.0/postProcessing/sets/30/'
+before = np.loadtxt(path + 'before_Ux_Uy.xy', delimiter=' ', unpack=True)
+after  = np.loadtxt(path + 'after_Ux_Uy.xy',  delimiter=' ', unpack=True)
+
+Y, UX = 0, 1
+before_Y,  after_Y  = before[Y],  after[Y]
+before_UX, after_UX = before[UX], after[UX]
+
+plt.figure(figsize=fig_dims)
+plt.plot(before_UX, before_Y, 'b+', label='before')
+plt.plot( after_UX,  after_Y, 'rx', label='after')
+plt.ylabel('y (m)')
+plt.xlabel('Error in Ux (m/s)')
+plt.legend(loc='upper left')
 plt.show()
